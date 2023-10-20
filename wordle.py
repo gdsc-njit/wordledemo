f2f1bfb8628bdab5446c47dbbe027328cd67c9ea
Google Developer Student Club @ NJIT <58719016+gdsc-njit@users.noreply.github.com>
2023-10-20 10:45:52 -0400

    ADD VARIABLES

--- wordle.py
+++ wordle.py
@@ -1,6 +1,9 @@
 from letter_state import LetterState
 
 class Wordle:
+    MAX_ATTEMPTS = 6
+    WORD_LENGTH = 5
+    
     def __init__(self, secret: str):
         self.secret = secret.upper()
         self.attempts = []
